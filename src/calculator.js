64df0c63cf930ad76646436627796ef28fe53c8e
Bernhard Göschlberger <bernhard.goeschlberger@researchstudio.at>
2021-11-17 13:46:04 +0100

    added input sanitation

--- src/calculator.js
+++ src/calculator.js
@@ -2,9 +2,22 @@ export class Calculator {
   constructor(){
     this._lastResult = 0;
   }
+  
+  _sanitizeInput(v){
+    if(!isNaN(v) && v.toString() !== ''){
+      if(typeof v === 'number'){
+        return v;
+      } else if (v.toString().indexOf('.') !== -1){
+        return Number.parseFloat(v);
+      } else {
+        return Number.parseInt(v);
+      }
+    }
+    throw 'Invalid Input: ' + JSON.stringify(v) + ' is not a number';
+  }
 
   add(v1, v2){
-     this._lastResult = v1+v2;
+     this._lastResult = this._sanitizeInput(v1)+this._sanitizeInput(v2);
      return this._lastResult;
   }
 
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,46 @@
 import { Calculator } from "./calculator.js";
 
  
-test('add 1 and 1 should be 2', () => {
+test('adding integers should work', () => {
   const calculator = new Calculator();
   expect(calculator.add(1,1)).toBe(2);
+});
+
+test('adding floats should work', () => {
+  const calculator = new Calculator();
+  expect(calculator.add(1.1,1.1)).toBe(2.2);
+  expect(calculator.add(1.1,1)).toBe(2.1);
+  expect(calculator.add(1,1.1)).toBe(2.1);
+});
+
+test('numeric strings should be coerced', () => {
+  const calculator = new Calculator();
+  expect(calculator.add('1', 1)).toBe(2);
+  expect(calculator.add('1.1', 1)).toBe(2.1);
+  expect(calculator.add(1, '1')).toBe(2);
+  expect(calculator.add(1, '1.1')).toBe(2.1);
+});
+
+test('non-numerical input should fail', () => {
+  const calculator = new Calculator();
+  expect(() => calculator.add('a', 1)).toThrow();
+  expect(() => calculator.add(1, 'a')).toThrow();
+});
+
+test('empty string should fail', () => {
+  const calculator = new Calculator();
+  expect(() => calculator.add('', 1)).toThrow();
+  expect(() => calculator.add(1, '')).toThrow();
+});
+
+test('undefined should fail', () => {
+  const calculator = new Calculator();
+  expect(() => calculator.add(undefined, 1)).toThrow();
+  expect(() => calculator.add(1, undefined)).toThrow();
+});
+
+test('null should fail', () => {
+  const calculator = new Calculator();
+  expect(() => calculator.add(null, 1)).toThrow();
+  expect(() => calculator.add(1, null)).toThrow();
 });
